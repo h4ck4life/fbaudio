506c81e1cd3059d8a6af1644160a36f57653acab
Alif <alifaziz@gmail.com>
2021-04-25 18:59:40 +0800

    fix radius

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/radius'));
+app.use(express.static('./dist/fbaudio'));
 
 app.get('/*', function (req, res) {
   res.sendFile('index.html', { root: 'dist/fbaudio' }
